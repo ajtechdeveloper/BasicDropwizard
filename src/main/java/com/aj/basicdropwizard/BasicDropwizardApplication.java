05f121b524de0c13c7b5cebf63de91fe3e306c43
Aj Tech Developer <techdeveloper.aj@gmail.com>
2017-07-09 01:42:48 +0530

    Code optimizations

--- src/main/java/com/aj/basicdropwizard/BasicDropwizardApplication.java
+++ src/main/java/com/aj/basicdropwizard/BasicDropwizardApplication.java
@@ -3,6 +3,7 @@ package com.aj.basicdropwizard;
 import com.aj.basicdropwizard.resource.BasicDropwizardHealthCheckResource;
 import com.aj.basicdropwizard.resource.PingResource;
 import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,10 +17,13 @@ public class BasicDropwizardApplication extends Application<BasicDropwizardConfi
 		new BasicDropwizardApplication().run("server", args[0]);
 	}
 
+    @Override
+    public void initialize(Bootstrap<BasicDropwizardConfiguration> b) {
+    }
+
 	@Override
 	public void run(BasicDropwizardConfiguration config, Environment env)
 			throws Exception {
-		BasicDropwizardConfigSettings.getInstance().init(config);
         logger.info("Registering RESTful API resources");
 		env.jersey().register(new PingResource());
 		env.healthChecks().register("BasicDropwizardHealthCheck",
